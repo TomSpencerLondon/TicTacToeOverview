003bd67b9166a3bb1b72395b960feb7874b19188
Tom <thomas.spencer@codurance.com>
2020-08-12 08:27:43 +0100

    Add Square occupied RuntimeException

--- src/main/java/com/codurance/TicTacToe.java
+++ src/main/java/com/codurance/TicTacToe.java
@@ -2,9 +2,22 @@ package com.codurance;
 
 public class TicTacToe {
 
+  private Character[][] board = {
+      new Character[]{'O', 'O', 'O'},
+      new Character[]{'O', 'O', 'O'},
+      new Character[]{'O', 'O', 'O'}
+  };
+
   public void play(int x, int y) {
     if (x < 1 || x > 3) {
-     throw new RuntimeException("X is outside board");
+      throw new RuntimeException("X is outside board");
+    } else if (y < 1 || y > 3) {
+      throw new RuntimeException("Y is outside board");
+    }
+    if (board[x - 1][y - 1] != 'O'){
+      throw new RuntimeException("Square occupied");
+    } else {
+      board[x - 1][y - 1] = 'X';
     }
   }
 }
--- src/test/java/com/codurance/TicTacToeShould.java
+++ src/test/java/com/codurance/TicTacToeShould.java
@@ -1,5 +1,8 @@
 package com.codurance;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.api.BDDAssertions.thenThrownBy;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -9,7 +12,6 @@ public class TicTacToeShould {
 
   private TicTacToe ticTacToe;
 
-
   @BeforeEach
   void setUp() {
     ticTacToe = new TicTacToe();
@@ -17,8 +19,21 @@ public class TicTacToeShould {
 
   @Test
   void not_allow_x_to_be_outside_of_the_board() {
-    assertThrows(RuntimeException.class, () -> {
-      ticTacToe.play(5, 2);
-    });
+    Exception exception = assertThrows(RuntimeException.class, () -> ticTacToe.play(5, 2));
+    assertThat(exception.getMessage()).isEqualTo("X is outside board");
+  }
+
+  @Test
+  void not_allow_y_to_be_outside_of_the_board() {
+    Exception exception = assertThrows(RuntimeException.class, () -> ticTacToe.play(2, 5));
+    assertThat(exception.getMessage()).isEqualTo("Y is outside board");
+  }
+
+  @Test
+  void not_allow_playing_the_same_square_twice() {
+    ticTacToe.play(1, 1);
+    thenThrownBy(() -> ticTacToe.play(1, 1))
+        .isInstanceOf(RuntimeException.class)
+        .hasMessage("Square occupied");
   }
 }
