799f323c6c841fc2c04849232a460836f757c55a
Tom <thomas.spencer@codurance.com>
2020-08-20 14:23:34 +0100

    Add test for tic tac toe

--- src/main/java/com/codurance/TicTacToe.java
+++ src/main/java/com/codurance/TicTacToe.java
@@ -2,22 +2,26 @@ package com.codurance;
 
 public class TicTacToe {
 
-  private Character[][] board = {
+  private final Character[][] board = {
       new Character[]{'O', 'O', 'O'},
       new Character[]{'O', 'O', 'O'},
       new Character[]{'O', 'O', 'O'}
   };
 
   public void play(int x, int y) {
-    if (x < 1 || x > 3) {
-      throw new RuntimeException("X is outside board");
-    } else if (y < 1 || y > 3) {
-      throw new RuntimeException("Y is outside board");
-    }
+    checkBounds(x, y);
     if (board[x - 1][y - 1] != 'O'){
       throw new RuntimeException("Square occupied");
     } else {
       board[x - 1][y - 1] = 'X';
+    }
+  }
+
+  private void checkBounds(int x, int y) {
+    if (x < 1 || x > 3) {
+      throw new RuntimeException("X is outside board");
+    } else if (y < 1 || y > 3) {
+      throw new RuntimeException("Y is outside board");
     }
   }
 }
--- src/test/java/com/codurance/TicTacToeShould.java
+++ src/test/java/com/codurance/TicTacToeShould.java
@@ -1,9 +1,7 @@
 package com.codurance;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.BDDAssertions.then;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.assertj.core.api.BDDAssertions.thenThrownBy;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -19,14 +17,16 @@ public class TicTacToeShould {
 
   @Test
   void not_allow_x_to_be_outside_of_the_board() {
-    Exception exception = assertThrows(RuntimeException.class, () -> ticTacToe.play(5, 2));
-    assertThat(exception.getMessage()).isEqualTo("X is outside board");
+    assertThatThrownBy(() -> ticTacToe.play(5, 2))
+        .isInstanceOf(RuntimeException.class)
+        .hasMessage("X is outside board");
   }
 
   @Test
   void not_allow_y_to_be_outside_of_the_board() {
-    Exception exception = assertThrows(RuntimeException.class, () -> ticTacToe.play(2, 5));
-    assertThat(exception.getMessage()).isEqualTo("Y is outside board");
+    assertThatThrownBy(() -> ticTacToe.play(2, 5))
+        .isInstanceOf(RuntimeException.class)
+        .hasMessage("Y is outside board");
   }
 
   @Test
